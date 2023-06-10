898d7b513006d3dc35d87a57aafe6e807ce4248a
jiaLEE <jia9510@naver.com>
2023-06-10 19:50:43 +0900

    factory method 방식으로 bean 생성, 등록하기.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -19,21 +23,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 
-
+@Configuration // spring에게 알린다. -> 해당 annotation은 가장 먼저 spring에게 등록당한다. 중요한 설정들이 많기떄문.
 public class HellobootApplication {
 
-	/**
-	 * springboot는 초기에 servlet container를 신경쓰지 않아도 알아서 서버가 뜬다.
-	 * 어떻게 가능한거지? -> 해당 어노테이션을 없애고 직접 servlet container를 띄워보자!
-	 * servlet은 java지원 프로그램 -> 먼저 Servlet Container를 띄운다 -> 띄우려면 tomcat을 실행해야하는데 어떻게 띄우지? -> tomcat로 java로 되어있음.
-	 */
+	@Bean // spring에게 bean 생성되는 로직인걸 알린다.
+	public HelloController helloController(HelloService helloService) { // spring이 알아서 주입하게끔 파라미터에 넣는다.
+		return new HelloController(helloService);
+	}
 
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 	public static void main(String[] args) {
 		// spring container가 초기화되는 시점에 Servelt contianer를 만든다.
 
 		// spring container 만들고 bean 등록 후 초기화한다.
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -49,8 +57,9 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+
+		// 코드단에서 bean을 생성했기때문에 register하는게 아니라 config가 있는 해당 클래스만을 알려준다.
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 	}
 
