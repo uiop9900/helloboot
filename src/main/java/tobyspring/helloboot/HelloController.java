90089f0253b3af570a2e11a0c617749e2eea11ed
jiaLEE <jia9510@naver.com>
2023-06-10 20:06:04 +0900

    @Component와 @ComponentScan.

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,15 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping
+@RestController
 public class HelloController {
     // helloService interface를 선언해서 생성자 주입받는다.
 
@@ -19,7 +22,6 @@ public class HelloController {
     // 여기서 mapping을 하게 되면 DispatcherServlet이 class단위, method단위로 뒤져서 맞는 bean을 찾아낸다.
     // 이때 class 단위로 먼저 찾기때문에 class에도 annotation을 붙인다.
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -25,28 +26,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 
-@Configuration // spring에게 알린다. -> 해당 annotation은 가장 먼저 spring에게 등록당한다. 중요한 설정들이 많기떄문.
+@Configuration
+@ComponentScan
 public class HellobootApplication {
-
-	@Bean // spring에게 bean 생성되는 로직인걸 알린다.
-	public HelloController helloController(HelloService helloService) { // spring이 알아서 주입하게끔 파라미터에 넣는다.
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
 	public static void main(String[] args) {
-		// spring container가 초기화되는 시점에 Servelt contianer를 만든다.
-
-		// spring container 만들고 bean 등록 후 초기화한다.
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
 
-				// servlet container를 만들어서 frontController 역할의 servlet을 등록한다.
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
@@ -58,7 +46,6 @@ public class HellobootApplication {
 			}
 		};
 
-		// 코드단에서 bean을 생성했기때문에 register하는게 아니라 config가 있는 해당 클래스만을 알려준다.
 		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 	}
