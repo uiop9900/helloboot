26f7d21fa956936e47e75dce9004ccdfe9f69137
jiaLEE <jia9510@naver.com>
2023-06-10 17:11:19 +0900

    코드 정리.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -23,7 +23,7 @@ public class HellobootApplication {
 	/**
 	 * springboot는 초기에 servlet container를 신경쓰지 않아도 알아서 서버가 뜬다.
 	 * 어떻게 가능한거지? -> 해당 어노테이션을 없애고 직접 servlet container를 띄워보자!
-	 * servlet은 java지원 프로그램 -> 먼저 Servlet Container를 띄운다 -> 띄우려면 tomcat을 실행해야하는데 어떻게 띄우지? -> tom
+	 * servlet은 java지원 프로그램 -> 먼저 Servlet Container를 띄운다 -> 띄우려면 tomcat을 실행해야하는데 어떻게 띄우지? -> tomcat로 java로 되어있음.
 	 */
 
 	public static void main(String[] args) {
@@ -36,17 +36,13 @@ public class HellobootApplication {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통기능
-					// 기존 servlet Container에서 매핑하던걸 이제는 fronController에서 매핑해야한다.
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
-						String ret = helloController.hello(name); // 바인딩: 직적으로 웹요청과 응답을 다루는것들을 넣지 않고 String(java Type)으로 변환해서 넣어준다.
+						String ret = helloController.hello(name);
 
-						resp.setStatus(HttpStatus.OK.value()); // 상태코드
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // header
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(ret); // body
-					} else if (req.getRequestURI().equals("/users")) {
-
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
